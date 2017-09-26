559f16bedd6fd367491c481f05eb51ef79f0ed52
manishkumar212111 <manish.Kumar212111@gmail.com>
2017-09-26 22:14:13 +0530

    Add files via upload

---
+++ LIBRARY.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include"library1.h"
+void main()
+{
+textcolor(4);
+textbackground(10);
+printf("\t\t\t.........................\n");
+printf("\t\t\t|  LIBRARY MANAGEMENT    |\n(Note:ID is primary key)\n");
+printf("\t\t\t.........................\n");
+ma_section();
+getch();
+}
